7f308c9a176e9743d435882d82cf29976e96cf5e
wusamin <tatsusamimi@gmail.com>
2019-07-07 22:10:54 +0900

    initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(3000);
+
+console.log('Server running at http://127.0.0.1:3000');
